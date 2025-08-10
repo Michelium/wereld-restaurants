7ccdd8f4bda269e10adfd10cf4b05b640fcef92b
Michel Hamelink <info@michelhamelink.nl>
2025-08-10 10:54:42 +0200

    feat: add MonologBundle to enhance logging capabilities

--- config/bundles.php
+++ config/bundles.php
@@ -14,4 +14,5 @@ return [
     Symfony\Bundle\SecurityBundle\SecurityBundle::class => ['all' => true],
     EasyCorp\Bundle\EasyAdminBundle\EasyAdminBundle::class => ['all' => true],
     Pentatrion\ViteBundle\PentatrionViteBundle::class => ['all' => true],
+    Symfony\Bundle\MonologBundle\MonologBundle::class => ['all' => true],
 ];
